a1f9834bbc8aabedd746bb8affd69530f7dacde1
Nikhil Bindal <nikhilbindal983@gmail.com>
2025-04-19 23:56:19 -0400

    eslint ignored during builds

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
     images: {
         domains: ["example.com"], // Add domains for external images if needed
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
